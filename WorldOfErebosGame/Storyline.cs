6055a490fa15e54cf1a867473134fe32b02580de
Jesrino <152989770+Jesrino@users.noreply.github.com>
2024-04-01 21:07:21 +0800

    Update Storyline.cs

--- WorldOfErebosGame/Storyline.cs
+++ WorldOfErebosGame/Storyline.cs
@@ -3,7 +3,7 @@ class Storyline
     World location = new World();
     public static void GameIntroduction()
     {
-
+Console.WriteLine("A hero summoned in a world full of magic comes back to his original world and finds it destroyed by human-eating monsters. He seeks revenge for his loved ones, using his retained strength and powers from the other world, yet to reach his full potential, making him a threat to the demons.");
     }
     public static void scene1()
     {
